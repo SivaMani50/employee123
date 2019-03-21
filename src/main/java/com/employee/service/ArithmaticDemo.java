9f985111fcfb64544c744ecfc572eb5170688239
SivaMani50 <mani.thota518@gmail.com>
2019-03-21 12:13:38 +0530

    ArithimaticException

---
+++ src/main/java/com/employee/service/ArithmaticDemo.java
@@ -0,0 +1,15 @@
+package com.employee.service;
+
+public class ArithmaticDemo {
+	public static void main(String[] args) {
+
+		try {
+			int a = 30, b = 0;
+			int c = a / b; // cannot divide by zero
+			System.out.println("Result = " + c);
+		} catch (ArithmeticException e) {
+			System.out.println("Can't divide a number by 0");
+		}
+	}
+
+}
